9ea39ad138c376a3fa47a9b601cca86f76d667b3
Omnia Ahmed <139129370+Moniaar@users.noreply.github.com>
2024-06-24 16:18:29 +0300

    searching for case sensitivity errors

--- 0x00-Webpack/task_2/webpack.config.js
+++ 0x00-Webpack/task_2/webpack.config.js
@@ -1,23 +1,21 @@
 const path = require('path');
 
 module.exports = {
-  entry: {
+    mode: 'production',
+    entry: {
         main: path.resolve(__dirname, './js/dashboard_main.js'),
     },
     output: {
         filename: 'bundle.js',
         path: path.resolve(__dirname, 'public')
     },
-  mode: 'production'
-  module: {
-    rules: [
-      {
-        test: /\.css$/,
-        use: ['style-loader', 'css-loader'],
-      },
-      {
-        test: /\.(png|jpg|jpeg|gif)$/i,
-          {
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader'],
+            },
+            {
                 test: /\.(png|svg|jpg|jpeg|gif)$/i,
                 use: [
                     'file-loader',
@@ -29,5 +27,7 @@ module.exports = {
                         },
                     },
                 ],
-          },
+            },
+        ],
+    },
 };
