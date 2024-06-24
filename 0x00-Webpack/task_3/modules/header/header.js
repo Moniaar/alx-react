958931f4de9efa15c22a360bde3e613388afd1a9
root <root@DESKTOP-4SRDOID.localdomain>
2024-06-24 17:32:19 +0300

    the main modules that creates this app bundle are all here

---
+++ 0x00-Webpack/task_3/modules/header/header.js
@@ -0,0 +1,8 @@
+import $ from 'jquery';
+import './header.css';
+import logo from '../../assets/holberton-logo.jpg';
+
+console.log('Init header');
+
+$('body').append('<div id="logo"></div>');
+$('body').append('<h1>Holberton Dashboard</h1>');
