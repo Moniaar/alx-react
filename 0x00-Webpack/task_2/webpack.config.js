ca16d446d98ef644b35fc4b8acc119cd834cc76f
root <root@DESKTOP-4SRDOID.localdomain>
2024-06-24 15:57:04 +0300

    configured now to handule both css and img files

---
+++ 0x00-Webpack/task_2/webpack.config.js
@@ -0,0 +1,27 @@
+const path = require('path');
+
+module.exports = {
+  entry: {
+        main: path.resolve(__dirname, './js/dashboard_main.js'),
+    },
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'public')
+    },
+  mode: 'production'
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
+    ],
+  },
+  performance: {
+    maxAssetSize: 512000,
+  },
+};
