8653b18e30f58928fd7477ee7a5bf09f0e3b640b
root <root@DESKTOP-4SRDOID.localdomain>
2024-06-27 18:28:21 +0300

    to make sure I puhsed everything in this directory

--- 0x01-react_intro/task_2/dashboard/src/index.js
+++ 0x01-react_intro/task_2/dashboard/src/index.js
@@ -1,19 +1,21 @@
 // src/index.js
-
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import Notifications from './Notifications';
 
-ReactDOM.render(
+const root = ReactDOM.createRoot(document.getElementById('root'));
+const rootNotif = ReactDOM.createRoot(document.getElementById('root-notifications'));
+
+root.render(
   <React.StrictMode>
-    <div id="root-app">
-      <App />
-    </div>
-    <div id="root-notifications">
-      <Notifications />
-    </div>
-  </React.StrictMode>,
-  document.getElementById('root')
+    <App />
+  </React.StrictMode>
+);
+
+rootNotif.render(
+  <React.StrictMode>
+    <Notifications />
+  </React.StrictMode>
 );
