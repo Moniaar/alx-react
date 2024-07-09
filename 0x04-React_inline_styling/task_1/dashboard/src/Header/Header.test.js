ee4502472ce5916fce348746c5511c3399b0f506
root <root@DESKTOP-4SRDOID.localdomain>
2024-07-09 21:26:05 +0300

    the test file for the header file

---
+++ 0x04-React_inline_styling/task_1/dashboard/src/Header/Header.test.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import Header from './Header';
+import logo from '../assets/Holberton-logo.jpg';
+
+// Configure Enzyme
+import Enzyme from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+
+Enzyme.configure({ adapter: new Adapter() });
+
+describe('Header Component', () => {
+  it('renders without crashing', () => {
+    shallow(<Header />);
+  });
+
+  it('renders an image and an h1 tag', () => {
+    const wrapper = shallow(<Header />);
+    expect(wrapper.find('img').prop('src')).toEqual(logo);
+    expect(wrapper.find('h1').text()).toEqual('School dashboard');
+  });
+
+  it('applies correct styles', () => {
+    const wrapper = shallow(<Header />);
+    expect(wrapper.find('div').hasClass('header')).toBe(true);
+    expect(wrapper.find('img').hasClass('logo')).toBe(true);
+    expect(wrapper.find('h1').hasClass('title')).toBe(true);
+  });
+});
