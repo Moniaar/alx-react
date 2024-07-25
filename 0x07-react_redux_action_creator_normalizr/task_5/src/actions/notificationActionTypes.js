443d6cb801d14242b9b9ffe8a6c304b8399cee9f
root <root@DESKTOP-4SRDOID.localdomain>
2024-07-25 07:53:51 +0300

    added 2 actions for the notifications

--- 0x07-react_redux_action_creator_normalizr/task_5/src/actions/notificationActionTypes.js
+++ 0x07-react_redux_action_creator_normalizr/task_5/src/actions/notificationActionTypes.js
@@ -1,2 +1,8 @@
+// Define filter states
+export const NotificationTypeFilters = {
+  DEFAULT: 'DEFAULT',
+  URGENT: 'URGENT',
+};
+
 export const MARK_AS_READ = "MARK_AS_READ";
 export const SET_TYPE_FILTER = "SET_TYPE_FILTER";
