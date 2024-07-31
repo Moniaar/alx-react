32aef0a58b809b40d4ed7c7425531d39a5843807
root <root@DESKTOP-4SRDOID.localdomain>
2024-07-31 07:58:54 +0300

    defining the test suite for the simple reducer

---
+++ 0x08-react_redux_reducer_selector/task_0/dashboard/src/reducers/uiReducer.test.js
@@ -0,0 +1,33 @@
+import uiReducer from './uiReducer';
+import {
+  DISPLAY_NOTIFICATION_DRAWER,
+  HIDE_NOTIFICATION_DRAWER,
+  LOGIN_SUCCESS,
+  LOGIN_FAILURE,
+  LOGOUT,
+  SELECT_COURSE
+} from '../actions/uiActionTypes';
+
+describe('uiReducer', () => {
+  const initialState = {
+    isNotificationDrawerVisible: false,
+    isUserLoggedIn: false,
+    user: {}
+  };
+
+  it('should return the initial state when no action is passed', () => {
+    expect(uiReducer(undefined, {})).toEqual(initialState);
+  });
+
+  it('should return the initial state when the action SELECT_COURSE is passed', () => {
+    expect(uiReducer(undefined, { type: SELECT_COURSE })).toEqual(initialState);
+  });
+
+  it('should change isNotificationDrawerVisible to true when the action DISPLAY_NOTIFICATION_DRAWER is passed', () => {
+    const expectedState = {
+      ...initialState,
+      isNotificationDrawerVisible: true
+    };
+    expect(uiReducer(initialState, { type: DISPLAY_NOTIFICATION_DRAWER })).toEqual(expectedState);
+  });
+});
