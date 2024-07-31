31651b43e8fa06b85abf5473502ac8f6ce017011
root <root@DESKTOP-4SRDOID.localdomain>
2024-07-31 07:58:00 +0300

    creating the UI reducer function

---
+++ 0x08-react_redux_reducer_selector/task_0/dashboard/src/reducers/uiReducer.js
@@ -0,0 +1,50 @@
+// Define the initial state
+const initialState = {
+  isNotificationDrawerVisible: false,
+  isUserLoggedIn: false,
+  user: {}
+};
+
+// Import action types
+import {
+  DISPLAY_NOTIFICATION_DRAWER,
+  HIDE_NOTIFICATION_DRAWER,
+  LOGIN_SUCCESS,
+  LOGIN_FAILURE,
+  LOGOUT
+} from '../actions/uiActionTypes';
+
+// Reducer function
+function uiReducer(state = initialState, action) {
+  switch (action.type) {
+    case DISPLAY_NOTIFICATION_DRAWER:
+      return {
+        ...state,
+        isNotificationDrawerVisible: true
+      };
+    case HIDE_NOTIFICATION_DRAWER:
+      return {
+        ...state,
+        isNotificationDrawerVisible: false
+      };
+    case LOGIN_SUCCESS:
+      return {
+        ...state,
+        isUserLoggedIn: true
+      };
+    case LOGIN_FAILURE:
+      return {
+        ...state,
+        isUserLoggedIn: false
+      };
+    case LOGOUT:
+      return {
+        ...state,
+        isUserLoggedIn: false
+      };
+    default:
+      return state;
+  }
+}
+
+export default uiReducer;
