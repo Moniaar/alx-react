6d4977675bb661d802d31e8a30c7e2dac7fe2249
root <root@DESKTOP-4SRDOID.localdomain>
2024-08-07 07:44:37 +0300

    Update index.js to configure Redux DevTools along with redux-thunk

---
+++ 0x09-react_redux_connectors_and_providers/task_3/dashboard/src/index.js
@@ -0,0 +1,24 @@
+// index.js
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { createStore, applyMiddleware, compose } from 'redux';
+import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
+import rootReducer from './reducers';  // Import your rootReducer
+
+// Create a store with Redux DevTools Extension support
+const store = createStore(
+  rootReducer,
+  compose(
+    applyMiddleware(thunk),
+    window.__REDUX_DEVTOOLS_EXTENSION__ ? window.__REDUX_DEVTOOLS_EXTENSION__() : (f) => f
+  )
+);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
